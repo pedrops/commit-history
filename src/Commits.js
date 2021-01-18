e5bf20deed3d7acd4fbd50b50b1566321af3120c
Pedro Monzon <zeroperu001@gmail.com>
2021-01-18 01:25:30 -0500

    Adding commit history columns

--- src/Commits.js
+++ src/Commits.js
@@ -1,12 +1,16 @@
 import React from "react";
 import axios from "axios";
-import { DataGrid } from '@material-ui/data-grid';
+import { DataGrid } from "@material-ui/data-grid";
 
 class Commits extends React.Component {
   state = {
     commits: [],
     columns : [
         {field: "sha", headerName: "SHA", with: 90},
+        {field: "authorName", headerName: "Author name", with: 90},
+        {field: "authorEmail", headerName: "Author email", with: 90},
+        {field: "message", headerName: "Message", with: 90},
+        {field: "date", headerName: "Date", with: 90},
     ]
   };
 
@@ -15,6 +19,10 @@ class Commits extends React.Component {
       const commits = res.data;
       commits.forEach(element => {
         element.id = Math.random();
+        element.authorName = element.commit.author.name;
+        element.authorEmail = element.commit.author.email;
+        element.date = element.commit.author.date;
+        element.message = element.commit.message;
       });
       this.setState({ commits });
     });
